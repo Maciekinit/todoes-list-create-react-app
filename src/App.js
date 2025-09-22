7b9bbac9d43f6489e7654448657029c5d3677701
Maciej Blaszczyk <programinghub64@gmail.com>
2025-09-22 18:00:20 +0200

    hideDoneTasks renamed to hideDone

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const tasks = [
     { id: 3, content: "Odebrać pranie", done: false },
 ];
 
-const hideDoneTasks = false;
+const hideDone = false;
 
 function App() {
     return (
@@ -26,10 +26,10 @@ function App() {
 
             <Section
                 title="Lista zadań"
-                body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+                body={<Tasks tasks={tasks} hideDone={hideDone} />
                 }
                 extraHeaderContent={
-                    <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+                    <Buttons tasks={tasks} hideDone={hideDone} />
                 }
             />
         </Container>
