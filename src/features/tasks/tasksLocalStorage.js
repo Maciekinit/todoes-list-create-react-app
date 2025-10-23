9ad1ae189ffe260ca491c0aabcd3e007090d89bb
Maciej Blaszczyk <programinghub64@gmail.com>
2025-10-23 12:01:45 +0200

    correcting typos

--- src/features/tasks/tasksLocalStorage.js
+++ src/features/tasks/tasksLocalStorage.js
@@ -1,7 +1,7 @@
 const localStorageKey = "tasks";
 
-export const sevaTasksInLocalStorage = tasks =>
+export const saveTasksInLocalStorage = tasks =>
     localStorage.setItem("tasks", JSON.stringify(tasks));
 
 export const getTasksFromLocalStorage = () =>
-    JSON.parse(localStorage.getItem("tasks")) || [];
+    JSON.parse(localStorage.getItem(localStorageKey)) || [];
