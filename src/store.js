821398e0f8536ad779fe472276a577ed180c9b3b
Maciej Blaszczyk <programinghub64@gmail.com>
2025-10-23 22:48:10 +0200

    formatting the code and adding the correct middleware

--- src/store.js
+++ src/store.js
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from "redux-saga";
 import tasksReducer from './features/tasks/tasksSlice';
 import rootSaga from "./rootSaga";
+
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
     reducer: {
         tasks: tasksReducer,
     },
-    middleware: [sagaMiddleware],
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware().concat(sagaMiddleware),
 });
 
 sagaMiddleware.run(rootSaga);
